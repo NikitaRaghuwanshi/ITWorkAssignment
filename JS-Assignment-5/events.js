8c6604948cff2902b89593241281cfaf9d61426d
NikitaRaghuwanshi <97503219+NikitaRaghuwanshi@users.noreply.github.com>
2022-01-31 14:14:44 +0530

    Add files via upload

---
+++ JS-Assignment-5/events.js
@@ -0,0 +1,78 @@
+/*Click Event Performed..*/
+function clicked()
+{
+
+}
+
+/*Double Click Event Performed..*/
+function doubleclicked()
+{
+
+}
+
+/*Right Click Event Performed..*/
+function rightclicked()
+{
+
+}
+
+/*Mouse over Event Performed..*/
+function mousehover()
+{
+
+}
+
+/*Mouse Out Event Performed..*/
+function mouseout()
+{
+
+}
+
+/*Mouse Down Event Performed..*/
+function mousedown()
+{
+
+
+}
+
+/*Mouse Up Event Performed..*/
+function mouseup()
+{
+
+}
+
+/*Key Press Event Performed..*/
+function keypress()
+{
+
+}
+
+/*Key Up Event Performed..*/
+function keyup()
+{
+
+}
+
+/*Load Event Performed..*/
+function load()
+{
+
+}
+
+/*Unload Event Performed..*/
+function unload()
+{
+
+}
+
+/*Resize Event Performed..*/
+function resize()
+{
+
+}
+
+/*Scroll Event Performed..*/
+function scroll()
+{
+
+}
