4ffd1b76ed4edcbd7a3acfd7420c0e1aa414295b
NikitaRaghuwanshi <97503219+NikitaRaghuwanshi@users.noreply.github.com>
2022-02-10 12:03:54 +0530

    Add files via upload

--- PHP Assignmet-8/program4.php
+++ PHP Assignmet-8/program4.php
@@ -3,6 +3,12 @@
 		color: red;
 		font-size: 20px;
 	}
+	h3{
+		color: blue;
+	}
+	b{
+		color: green;
+	}
 </style>	
 <?php
 	echo "<span class='Que'>4] Example for interface ?</span>";
@@ -11,12 +17,28 @@
 		function emp_details($employee_name, $employee_salary);
 	}		
 	interface Department{
-		function dept_details($Department_name, $Department_head)
+		function dept_details($Department_name, $Department_head);
 	}
 	class Company implements Employee, Department{
-		function emp_details($employee_name, $employee_salary){
+		public function emp_details($employee_name, $employee_salary){
 			echo "<h3>Employee Details: </h3>";
-			echo "Company ";
+			echo "<b>Employee Name: </b> $employee_name<br>";
+			echo "<b>Employee Salary: </b>$employee_salary<br>";
+		}
+		public function dept_details($Department_name, $Department_head){
+			echo "<h3>Department Details: </h3>";
+			echo "<b>Department Name: </b>$Department_name<br>";
+			echo "<b>Department Head: </b>$Department_head<br>";
 		}
 	} 
+	$company=new Company();
+	$company->emp_details('Nikita','50,000');
+	$company->emp_details('Trupti','50,000');
+	$company->emp_details('Aishwariya','50,000');
+	$company->emp_details('Narsing','50,000');
+	$company->emp_details('Priya','50,000');
+	$company->dept_details('Kumudini','Full Stack Developer');
+	$company->dept_details('Dinesh','Production Manager');
+	$company->dept_details('Ankush','Developer');
+
 ?>
--- PHP Assignmet-8/program6.php
+++ PHP Assignmet-8/program6.php
@@ -3,9 +3,69 @@
 		color: red;
 		font-size: 20px;
 	}
+	b{
+		color:green;
+	}
+	h3{
+		color: blue;
+	}
 </style>
 <?php
 	echo "<span class='Que'>6] Use acceess modifiers ?</span>";
 
+	/*class Access{
+		public $name;
+
+		public function __construct($name){
+			$this->name=$name;
+		}
+		public function show(){
+			echo "<br><h3>Public Access Modifiers..</h3>";
+			echo "<b>Student Name: </b>".$this->name."<br>";
+		}
+	}
+	$student=new Access("Nikita Raghuwanshi");
+	//$student->name="Akash"; //Public Access Modifier use
+	$student->show();
+*/
+	/*class Access{
+		protected $name;
+
+		public function __construct($name){
+			$this->name=$name;
+		}
+		protected function show(){
+			echo "<br><h3>Protected Access Modifiers..</h3>";
+			echo "<b>Student Name: </b>".$this->name."<br>";
+		}
+	}
+	class Modifier extends Access{
+		public function show(){
+			echo "<br><br><b>School Name: </b>".$this->name."<br>";
+		}
+	}
+	$student=new Access("Trupti Khaire");
+	//$student->name="Akash"; //Protected Access Modifier use
+	$student->show();*/
+
+	class Access{
+		private $name;
+
+		public function __construct($name){
+			$this->name=$name;
+		}
+		public function show(){
+			echo "<br><h3>Private Access Modifiers..</h3>";
+			echo "<b>Student Name: </b>".$this->name."<br>";
+		}	
+	}
+	class Modifier extends Access{
+		public function show(){
+			echo "<br><br><b>School Name: </b>".$this->name."<br>";
+		}
+	}
+	$student=new Modifier("Trupti Khaire");
+	//$student->name="Akash"; //Protected Access Modifier use
+	$student->show();
 			
 ?>
